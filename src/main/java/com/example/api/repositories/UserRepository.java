0236b8fe6a8602b55ecdaba026826f1462f72e33
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-02-20 18:49:32 -0300

    UserRepository atualizado.

---
+++ src/main/java/com/example/api/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.api.repositories;
+
+import com.example.api.domains.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+
+    Mono<User> findByUsername(String username);
+
+}
